287b0b65d04184ecc7fdc1629d4a145fecc7a32e
Ben Rencher <rencherb@byu.edu>
2025-07-18 12:10:00 -0600

    modifying auth_token request method to build bearer token string

--- core/api.py
+++ core/api.py
@@ -4,7 +4,9 @@ import requests
 
 
 # a valid auth token is required to access data from OIT APIs
-def get_auth_token(client_id, client_secret):
+# this token is packaged as a "Bearer" token in the "Authorization"
+# request header. This method builds and returns that Bearer token header
+def build_auth_header(client_id, client_secret):
     # request an auth token from OIT's auth token granting endpoint
     token_request = requests.post(
         "https://api.byu.edu/token",
@@ -13,4 +15,5 @@ def get_auth_token(client_id, client_secret):
     )
     token_json_result = token_request.json()
     access_token = token_json_result["access_token"]
-    return access_token
+    auth_header = f"Bearer {access_token}"
+    return auth_header
