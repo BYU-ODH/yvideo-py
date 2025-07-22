95ac49af7e28deb42c5d130a1b12f793d5a46ac8
Ben Rencher <rencherb@byu.edu>
2025-07-22 11:22:59 -0600

    updating api calls so urls go in secret_settings and we determine if current yearterm is close to ending

--- core/api.py
+++ core/api.py
@@ -1,8 +1,10 @@
 # This file defines the functions used to request data from OIT's APIs
 
-import requests
 from datetime import datetime
 from datetime import timedelta
+
+import requests
+
 import yvideo.secret_settings as secret_settings
 from .models import AuthToken
 
@@ -32,9 +34,9 @@ class Api:
     def generate_auth_token(self):
         # request an auth token from OIT's auth token granting endpoint
         token_request = requests.post(
-            secret_settings.AUTH_TOKEN_API_URL,
+            secret_settings.API_AUTH_TOKEN_URL,
             data={"grant_type": "client_credentials"},
-            auth=(secret_settings.CLIENT_ID, secret_settings.CLIENT_SECRET),
+            auth=(secret_settings.API_CLIENT_ID, secret_settings.API_CLIENT_SECRET),
         )
         token_json_result = token_request.json()
         return token_json_result["access_token"]
@@ -43,12 +45,36 @@ class Api:
         auth_header = f"Bearer {self.auth_token}"
         return auth_header
 
+    def calculate_next_year_term(self, yearterm_string):
+        year_string = yearterm_string[:4]
+        term_string = yearterm_string[4:]
+        new_year_string = None
+        new_term_string = None
+        # Fall to Winter
+        if term_string == "5":
+            new_year_string = str(int(year_string) + 1)
+            new_term_string = "1"
+        # Winter to Spring
+        elif term_string == "1":
+            new_year_string = year_string
+            new_term_string = "3"
+        # Spring to Summer
+        elif term_string == "3":
+            new_year_string = year_string
+            new_term_string = "4"
+        # Summer to Fall
+        elif term_string == "4":
+            new_year_string = year_string
+            new_term_string = "5"
+
+        return new_year_string + new_term_string
+
     def get_current_year_term(self):
         # to determine current year term, we have to compare to today's date
         today_datetime = datetime.today().strftime("%Y-%m-%dT%H:%M:%S")
 
         # get yearterm information
-        url = secret_settings.YEARTERM_API_URL
+        url = secret_settings.API_YEARTERM_URL
         headers = {"Authorization": self.build_auth_header()}
         control_date_request = requests.get(url, headers=headers)
         control_date_json_response = control_date_request.json()
@@ -56,12 +82,20 @@ class Api:
 
         # determine which yearterm corresponds to current datetime
         yearterm = None
+        is_two_weeks_from_end = False
         for entry in response_data:
             if (
                 entry["start_date_time"] <= today_datetime
                 and entry["end_date_time"] > today_datetime
             ):
                 yearterm = entry["year_term"]
+                yearterm_end_datetime = datetime.strptime(
+                    entry["end_date_time"], "%Y-%m-%dT%H:%M:%S"
+                )
+                # determine if the end of the current year term is 2 weeks or less away
+                two_weeks_from_end = yearterm_end_datetime - timedelta(days=14)
+                is_two_weeks_from_end = datetime.now() >= two_weeks_from_end
+
                 break
 
-        return yearterm
+        return {"yearterm": yearterm, "is_two_weeks_from_end": is_two_weeks_from_end}
--- core/tests.py
+++ core/tests.py
@@ -15,4 +15,15 @@ class ApiTests(TestCase):
         re = r"[0-9]{4}[1-6]"
         new_api = api.Api()
         result = new_api.get_current_year_term()
-        self.assertRegex(result, re)
+        self.assertRegex(result["yearterm"], re)
+
+    def test_calculate_next_year_term(self):
+        new_api = api.Api()
+        fall_to_winter = new_api.calculate_next_year_term("20255")
+        self.assertEqual(fall_to_winter, "20261")
+        winter_to_spring = new_api.calculate_next_year_term("20261")
+        self.assertEqual(winter_to_spring, "20263")
+        spring_to_summer = new_api.calculate_next_year_term("20263")
+        self.assertEqual(spring_to_summer, "20264")
+        summer_to_fall = new_api.calculate_next_year_term("20264")
+        self.assertEqual(summer_to_fall, "20265")
