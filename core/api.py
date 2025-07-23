c08ed42925f9f0cf88b51719ad8493dfd5ac1e2f
Ben Rencher <80055175+benrencher@users.noreply.github.com>
2025-07-22 20:01:35 -0600

    adding student and employee summary api endpoints (#10)

--- core/api.py
+++ core/api.py
@@ -99,3 +99,75 @@ class Api:
                 break
 
         return {"yearterm": yearterm, "is_two_weeks_from_end": is_two_weeks_from_end}
+
+    def get_worker_id_from_byu_id(self, byu_id):
+        url = secret_settings.API_WORKER_ID_IAM_URL + "?byu_id=" + byu_id
+        headers = {"Authorization": self.build_auth_header()}
+        worker_id_request = requests.get(url, headers=headers)
+        worker_id_json_response = worker_id_request.json()
+        response_data = worker_id_json_response["data"]
+        worker_id = None
+        if len(response_data) > 0:
+            worker_id = response_data[0]["worker_id"]
+
+        return worker_id
+
+    # byu_id is passed into this so we can record it later. byu_id does not return in the response.
+    def get_worker_summary(self, worker_id, byu_id):
+        url = secret_settings.API_WORKER_SUMMARY_URL + "?worker_id=" + worker_id
+        headers = {"Authorization": self.build_auth_header()}
+        summary_request = requests.get(url, headers=headers)
+        summary_json_res = summary_request.json()
+        response_data = summary_json_res["data"]
+        parsed_summary = {
+            "first_name": "",
+            "last_name": "",
+            "email": "",
+            "is_faculty": False,
+            "worker_id": worker_id,
+            "byu_id": byu_id,
+        }
+        if len(response_data) > 0:
+            data = response_data[0]
+            worker_positions = data["positions"]
+            parsed_summary["first_name"] = data["preferred_first_name"]
+            parsed_summary["last_name"] = data["preferred_last_name"]
+            parsed_summary["email"] = data["work_email_address"]
+            faculty_keyword = "faculty"
+            for position in worker_positions:
+                position_code = position[
+                    "employee_or_contingent_worker_type_reference_id"
+                ]
+                position_profile = position["job_profile"].lower()
+                position_title = position["business_title"].lower()
+                if (
+                    position_code == "FAC"
+                    or faculty_keyword in position_profile
+                    or faculty_keyword in position_title
+                ):
+                    parsed_summary["is_faculty"] = True
+
+            return parsed_summary
+        else:
+            return None
+
+    def get_student_summary(self, byu_id):
+        url = secret_settings.API_STUDENT_SUMMARY_URL + "?byu_id=" + byu_id
+        headers = {"Authorization": self.build_auth_header()}
+        summary_request = requests.get(url, headers=headers)
+        summary_json_res = summary_request.json()
+        data = summary_json_res["data"]
+        if len(data) > 0:
+            data = data[0]
+            preferred_first_name = data["preferred_name"].split(" ")[0]
+            parsed_summary = {
+                "first_name": preferred_first_name,
+                "last_name": data["preferred_last_name"],
+                "email": data["student_email_address"],
+                "is_faculty": False,
+                "byu_id": byu_id,
+                "net_id": data["net_id"],
+            }
+            return parsed_summary
+        else:
+            return None
