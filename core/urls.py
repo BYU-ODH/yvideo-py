f9781743b1d5c7b010e33be8c30c38aed8779eb3
rosesxmarie <140836653+rosesxmarie@users.noreply.github.com>
2025-08-12 12:42:59 -0600

    landing page (#26)

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import create_collection
 from .views import index
+from .views import manage_collections
 from .views import player
 from .views import stream_file
 
@@ -8,6 +10,8 @@ app_name = "core"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manage-collections/", manage_collections, name="manage_collections"),
+    path("collections/create/", create_collection, name="create_collection"),
     path("player/<int:content_id>", player, name="player"),
     path("stream/<int:file_key>", stream_file, name="stream_file"),
 ]
