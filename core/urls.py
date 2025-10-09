7e2dcd05ce57464741bfb46b28c97e5758042ce7
rosesxmarie <140836653+rosesxmarie@users.noreply.github.com>
2025-10-09 10:12:23 -0600

    skip mute blank annotations (#52)
    * skip mute blank annotations
    
    * Fixed ruff error
    
    * fixed the user

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views import add_annotation
 from .views import create_collection
 from .views import index
 from .views import invalid_login
@@ -15,5 +16,10 @@ urlpatterns = [
     path("collections/create/", create_collection, name="create_collection"),
     path("player/<int:content_id>", player, name="player"),
     path("stream/<int:file_key>", stream_file, name="stream_file"),
+    path(
+        "add_annotation/<str:annotation_type>/<int:file_id>/",
+        add_annotation,
+        name="add_annotation",
+    ),
     path("invalid-login", invalid_login, name="invalid_login"),
 ]
