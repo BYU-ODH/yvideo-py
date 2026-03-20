a94645254f0c82fadc5c97c9e4d2b84967155712
Robert Reynolds <reynoldsnlp@users.noreply.github.com>
2026-03-20 13:41:38 -0600

    168 rename re variable to pattern in ApiTests

--- core/tests.py
+++ core/tests.py
@@ -17,16 +17,16 @@ class ApiTests(TestCase):
     @unittest.expectedFailure
     def test_build_auth_header(self):
         new_api = api.Api()
-        re = r"Bearer[ ]\S*"
+        pattern = r"Bearer[ ]\S*"
         result = new_api.build_auth_header()
-        self.assertRegex(result, re)
+        self.assertRegex(result, pattern)
 
     @unittest.expectedFailure
     def test_get_current_year_term(self):
-        re = r"[0-9]{4}[1-6]"
+        pattern = r"[0-9]{4}[1-6]"
         new_api = api.Api()
         result = new_api.get_current_year_term()
-        self.assertRegex(result["yearterm"], re)
+        self.assertRegex(result["yearterm"], pattern)
 
     @unittest.expectedFailure
     def test_calculate_next_year_term(self):
