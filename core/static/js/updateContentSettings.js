0dce90048b2e6c56b59a867f1e0ab20e910a8b7d
Ben Rencher <rencherb@byu.edu>
2026-06-04 14:54:56 -0600

    adding reset functionality for content settings form

--- core/static/js/updateContentSettings.js
+++ core/static/js/updateContentSettings.js
@@ -1,5 +1,26 @@
-function initialize() {
+function setupReset() {
+  const resetButton = document.getElementById("content-settings-reset-button");
+  const contentIdInput = document.querySelector("input[name='id']");
+  if (!resetButton || !contentIdInput) {
+    console.error("resetButton or contentIdInput are not defined");
+    return;
+  }
+  resetButton.addEventListener("click", async () => {
+    const contentId = contentIdInput.value;
+    const resetResponse = await fetch(`/content/render-settings-form/${contentId}/`);
+    if (!resetResponse.ok) {
+      console.error("Failed to reset content settings form");
+      return;
+    }
 
+    const newFormHTML = await resetResponse.text();
+    const oldForm = document.getElementById("content-settings-form");
+    oldForm.outerHTML = newFormHTML;
+  });
+}
+
+function initialize() {
+  setupReset();
 }
 
 initialize();
