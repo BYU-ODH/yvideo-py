1a160f4cd45ee5cc03449f68687a28e6cca493b1
Ben Rencher <rencherb@byu.edu>
2026-06-11 11:17:48 -0600

    including api call to get_net_id_from_worker_id that we can turn on when ready

--- yvideo/odhOIDCAuthenticationBackend.py
+++ yvideo/odhOIDCAuthenticationBackend.py
@@ -1,24 +1,54 @@
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
+from core.api import Api
 from core.models import User
 
 
 class OIDCUserAuth(OIDCAuthenticationBackend):
     def verify_claims(self, claims):
-        if "byu_id" in claims and "netid" in claims:
+        if "byu_id" in claims or "netid" in claims:
             return True
 
     def create_user(self, claims):
         user = super().create_user(claims)
-        user.netid = claims.get("netid")
-        user.byu_id = claims.get("byu_id")
+        netid = claims.get("netid")
+        byu_id = claims.get("byu_id")
+        if netid is not None and byu_id is not None:
+            user.netid = netid
+            user.byu_id = byu_id
+        elif byu_id is not None:
+            api = Api()
+            student_summary = api.get_student_summary(byu_id)
+            if student_summary is not None:
+                user.netid = student_summary["net_id"]
+                user.byu_id = byu_id
+            else:
+                # this must be an employee/facutly, or is not effectively affiliated with BYU
+                worker_id = api.get_worker_id_from_byu_id(byu_id)
+                if worker_id is None:
+                    # this is unlikely to happen, but don't create a user for this person
+                    # because they are not a student, and are not an employee/faculty
+                    user.delete()
+                    return
+                # we don't get netid from worker_summary, but we can get the first and last name
+                #
+                # Only activate the following lines after we get access to this API - BDR 6/11/2026
+                # netid = api.get_net_id_from_worker_id(self, worker_id)
+                # user.netid = netid
+                # user.byu_id = byu_id
+        else:
+            # somehow this person got to this point without a valid byuid. Not sure how that could happen
+            # but don't build a user for this person/entity
+            user.delete()
+            return
+
         user.save()
         return user
 
     def update_user(self, user, claims):
-        user.netid = claims.get("netid")
-        user.byu_id = claims.get("byu_id")
-        user.save()
+        # for now, we don't do anything to update the user
+        # in the future, we may want to update the user's enrolled
+        # courses as this point, if they are a student - BDR 6/10/2026
         return user
 
     def filter_users_by_claims(self, claims):
