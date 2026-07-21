5ea5529c71167621f6b0cd24ea233a709e8f63b6
Rob Reynolds <reynoldsnlp@users.noreply.github.com>
2026-07-21 11:03:54 -0600

    refactor template paths (#281)

--- yvideo/settings.py
+++ yvideo/settings.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = "yvideo.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "core/templates/core"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
