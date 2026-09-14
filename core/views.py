9d88b70a74b3cba2dab7cbe6d1676f81c590f913
Ben Rencher <80055175+benrencher@users.noreply.github.com>
2026-09-14 11:19:31 -0600

    prevent archived playlist from being published. And automatically unp… (#416)
    * prevent archived playlist from being published. And automatically unpublish a playlist marked as archived
    
    * removing debug line

--- core/views.py
+++ core/views.py
@@ -693,8 +693,11 @@ def update_playlist_settings(request, playlist):
     if form.is_valid():
         try:
             playlist.name = form.cleaned_data["name"]
-            playlist.published = form.cleaned_data["published"]
             playlist.archived = form.cleaned_data["archived"]
+            if form.cleaned_data["archived"]:
+                playlist.published = False
+            else:
+                playlist.published = form.cleaned_data["published"]
             playlist.save()
             return render_playlist_info(request, playlist)
         except Exception as e:
